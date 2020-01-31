e3140c2c85366e2da6d074ea4654a10ef44e737d
venky5954 <59354036+venky5954@users.noreply.github.com>
2020-01-31 22:29:39 +0530

    Update App.java

--- src/main/java/mavenproject3/App.java
+++ src/main/java/mavenproject3/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         float first = 1.20f, second = 2.45f;
         System.out.println("--Before swap--");
-        System.out.println("First number = "  first);
+        System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
         // Value of first is assigned to temporary
         float temporary = first;
